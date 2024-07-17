d10caeb3a2ac82a24d7d81b9f5b3d402456fbe30
theofournier <fournier.theo@gmail.com>
2024-07-16 21:07:23 -0400

    Add Supabase

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,3 +1,16 @@
 export default function GalleryPage() {
-  return <h1>GALLERY</h1>;
+  return (
+    <div>
+      <h1>GALLERY</h1>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          {[...Array(20)].map((_, i) => (
+            <div key={i} className="bg-gray-300 p-4">
+              <img src="/home-img.webp" alt=" 1" className="w-full h-auto" />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
