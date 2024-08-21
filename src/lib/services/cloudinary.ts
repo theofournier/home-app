1521e81f14f3b88a0dd7fb9b5378a23dc370a163
theofournier <fournier.theo@gmail.com>
2024-08-21 14:23:19 -0400

    Fix cloudinary config

--- src/lib/services/cloudinary.ts
+++ src/lib/services/cloudinary.ts
@@ -5,7 +5,7 @@ import {
 } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
